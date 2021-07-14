a359c94dc686b5c5e175e260e7111ae6afa7e835
helloitsjoe <jboyle617@gmail.com>
2021-07-14 09:22:39 -0400

    Threads and macros

--- rustlings/exercises/macros/macros3.rs
+++ rustlings/exercises/macros/macros3.rs
@@ -2,12 +2,11 @@
 // Make me compile, without taking the macro out of the module!
 // Execute `rustlings hint macros3` for hints :)
 
-// I AM NOT DONE
-
+#[macro_use]
 mod macros {
     macro_rules! my_macro {
         () => {
-            println!("Check out my macro!");
+            println!("check out my macro!");
         };
     }
 }
--- rustlings/exercises/quiz4.rs
+++ rustlings/exercises/quiz4.rs
@@ -5,7 +5,12 @@
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
+#[macro_use]
+macro_rules! my_macro {
+    ($val:expr) => {
+        String::from("Hello ") + $val
+    };
+}
 
 #[cfg(test)]
 mod tests {
