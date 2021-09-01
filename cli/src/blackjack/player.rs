d3984194d3c1c1cd6811a3cdab0d7a95657b9b26
helloitsjoe <jboyle617@gmail.com>
2021-09-01 08:52:09 -0400

    Dealer play

--- cli/src/blackjack/player.rs
+++ cli/src/blackjack/player.rs
@@ -2,9 +2,12 @@ use super::deck::{Card, Deck};
 
 #[derive(Debug)]
 pub enum PlayerState {
+  Bust,
+  Stand,
+  Playing,
+  Blackjack,
   Won,
   Lost,
-  Playing,
 }
 
 pub struct Player {
@@ -44,6 +47,21 @@ impl Player {
     self.state = self.get_state(self.total);
   }
 
+  pub fn play(&mut self, deck: &mut Deck) {
+    // TODO: use traits to separate dealer/player behavior.
+    // Only dealer should implement play()
+    if self.total == 17 || self.total == 21 {
+      self.state = PlayerState::Stand;
+      return;
+    }
+    self.hit(deck.deal_one());
+    if self.total > 21 {
+      self.state = PlayerState::Bust;
+      return;
+    }
+    self.play(deck);
+  }
+
   fn score_cards(&self, cards: Vec<Card>) -> u32 {
     // TODO: Handle aces
     cards.into_iter().fold(0, |sum, card| card.val + sum)
@@ -52,35 +70,78 @@ impl Player {
   pub fn get_state(&self, total: u32) -> PlayerState {
     match total {
       1..=20 => PlayerState::Playing,
-      21 => PlayerState::Won,
+      21 => PlayerState::Blackjack,
       _ => PlayerState::Lost,
     }
   }
 }
 
-#[cfg(test)]
-mod test_player {
-  use super::*;
-
-  #[test]
-  fn test_player_hit() {
-    let mut player = Player::new(false);
-    player.hit(Card::new(3));
-    assert_eq!(player.hand.len(), 1);
-    player.hit(Card::new(4));
-    assert_eq!(player.hand.len(), 2);
-    assert_eq!(player.total, 7);
-  }
-
-  #[test]
-  fn test_player_state() {
-    let mut player = Player::new(false);
-    player.hit(Card::new(10));
-    player.hit(Card::new(10));
-    assert!(matches!(player.state, PlayerState::Playing));
-    player.hit(Card::new(1));
-    assert!(matches!(player.state, PlayerState::Won));
-    player.hit(Card::new(1));
-    assert!(matches!(player.state, PlayerState::Lost));
-  }
+#[test]
+fn player_hit() {
+  let mut player = Player::new(false);
+  player.hit(Card::new(3));
+  assert_eq!(player.hand.len(), 1);
+  player.hit(Card::new(4));
+  assert_eq!(player.hand.len(), 2);
+  assert_eq!(player.total, 7);
 }
+
+#[test]
+fn player_state() {
+  let mut player = Player::new(false);
+  player.hit(Card::new(10));
+  player.hit(Card::new(10));
+  assert!(matches!(player.state, PlayerState::Playing));
+  player.hit(Card::new(1));
+  assert!(matches!(player.state, PlayerState::Blackjack));
+  player.hit(Card::new(1));
+  assert!(matches!(player.state, PlayerState::Lost));
+}
+
+#[test]
+fn dealer_stand_on_21() {
+  let mut dealer = Player::new(true);
+  // TODO: Pass in Vec<Card> instead of length
+  let mut deck = Deck::new(Some(6));
+  dealer.deal(&mut deck);
+  println!("State: {:?}", dealer.state);
+  assert!(matches!(dealer.state, PlayerState::Playing));
+  dealer.play(&mut deck);
+  assert!(matches!(dealer.state, PlayerState::Stand))
+}
+
+#[test]
+fn dealer_bust() {
+  let mut dealer = Player::new(true);
+  // TODO: Pass in Vec<Card> instead of length
+  let mut deck = Deck::new(Some(7));
+  dealer.deal(&mut deck);
+  println!("State: {:?}", dealer.state);
+  assert!(matches!(dealer.state, PlayerState::Playing));
+  dealer.play(&mut deck);
+  assert!(matches!(dealer.state, PlayerState::Bust))
+}
+
+#[test]
+fn dealer_stand_on_17() {
+  let mut dealer = Player::new(true);
+  // TODO: Pass in Vec<Card> instead of length
+  let mut deck = Deck::new(Some(9));
+  dealer.deal(&mut deck);
+  println!("State: {:?}", dealer.state);
+  assert!(matches!(dealer.state, PlayerState::Playing));
+  dealer.play(&mut deck);
+  assert!(matches!(dealer.state, PlayerState::Stand))
+}
+
+// #[test]
+// fn dealer_hit_on_16() {
+//   let mut dealer = Player::new(true);
+//   // TODO: Pass in Vec<Card> instead of length
+//   let mut deck = Deck::new(Some(9));
+//   dealer.deal(&mut deck);
+//   println!("State: {:?}", dealer.state);
+//   assert!(matches!(dealer.state, PlayerState::Playing));
+//   dealer.play(&mut deck);
+//   assert!(matches!(dealer.state, PlayerState::Stand))
+// }
