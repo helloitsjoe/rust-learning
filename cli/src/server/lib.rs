113b36f57edfa733ea4121256072ffc64a5733dd
helloitsjoe <jboyle617@gmail.com>
2021-09-22 09:29:14 -0400

    create channel to send closures to threads

--- cli/src/server/lib.rs
+++ cli/src/server/lib.rs
@@ -1,8 +1,10 @@
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::thread::JoinHandle;
 
 pub struct ThreadPool {
   workers: Vec<Worker>,
+  sender: mpsc::Sender<Job>,
 }
 
 impl ThreadPool {
@@ -10,13 +12,22 @@ impl ThreadPool {
   pub fn new(size: usize) -> ThreadPool {
     assert!(size > 0);
 
+    let (sender, receiver) = mpsc::channel();
+
+    // Need Arc/Mutex to share receiver across threads. From the tutorial:
+    // "We put the receiving end of the channel in an Arc and a Mutex.
+    // For each new worker, we clone the Arc to bump the reference count
+    // so the workers can share ownership of the receiving end."
+    let receiver = Arc::new(Mutex::new(receiver));
+
     let mut workers = Vec::with_capacity(size);
 
     for id in 0..size {
-      workers.push(Worker::new(id));
+      workers.push(Worker::new(id, Arc::clone(&receiver)));
     }
 
-    ThreadPool { workers }
+    // Hold the sending end of the channel in the ThreadPool
+    ThreadPool { workers, sender }
   }
 
   pub fn execute<F>(&mut self, func: F)
@@ -27,14 +38,20 @@ impl ThreadPool {
   }
 }
 
+struct Job {
+  //
+}
+
 struct Worker {
   id: usize,
   thread: JoinHandle<()>,
 }
 
 impl Worker {
-  fn new(id: usize) -> Worker {
-    let thread = thread::spawn(|| {});
+  fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    let thread = thread::spawn(|| {
+      receiver;
+    });
     Worker { id, thread }
   }
 }
