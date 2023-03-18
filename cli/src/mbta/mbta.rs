e3d438b15969dac80720d1553dcf96a69054b8ae
helloitsjoe <jboyle617@gmail.com>
2023-03-17 22:43:37 -0400

    JSON

--- cli/src/mbta/mbta.rs
+++ cli/src/mbta/mbta.rs
@@ -1,6 +1,14 @@
 use super::super::Input;
+use serde::Deserialize;
+use serde_json::Value;
+use std::error::Error;
 
 pub struct MBTA {}
+
+#[derive(Deserialize)]
+struct MbtaResponse {
+    data: Value,
+}
 
 impl MBTA {
     pub fn new() -> MBTA {
@@ -9,28 +17,26 @@ impl MBTA {
 
     pub fn start(self, input: &mut Input) {
         println!("Hi");
-        self.handle_input(input);
+        let result = self.handle_input(input);
+        println!("{:?}", result);
+        if let Err(e) = result {
+            panic!("{:?}", e);
+        }
     }
 
-    pub fn handle_input(self, input: &mut Input) {
+    pub fn handle_input(self, input: &mut Input) -> Result<(), Box<dyn Error>> {
         println!("Please enter a route");
         let binding = input.get_input().to_lowercase();
         let route = binding.as_str();
         println!("Route: {:?}", route);
         // Request route
 
-        let res = reqwest::blocking::get("https://api-v3.mbta.com/routes/Red");
+        let res = reqwest::blocking::get("https://api-v3.mbta.com/routes/Red")?.text()?;
 
-        if let Err(e) = res {
-            println!("{:?}", e.status());
-        } else {
-            let text = res.unwrap().text();
+        println!("{:?}", res);
+        let json: MbtaResponse = serde_json::from_str(res.as_str())?;
 
-            if let Err(e) = text {
-                println!("{:?}", e);
-            } else {
-                println!("{:?}", text.unwrap());
-            }
-        }
+        println!("{:#?}", json.data);
+        return Ok(());
     }
 }
